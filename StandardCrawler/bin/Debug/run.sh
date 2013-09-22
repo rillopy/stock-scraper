8664718e7ef38177ee43bafae0255c8ee97ac870
rillopy <rillopy.storage@gmail.com>
2013-09-21 22:04:48 -0400

    removing debug folders

--- StandardCrawler/bin/Debug/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-_now=$(date +"%m_%d_%Y")
-_file="/home/rillopy/sp500_$_now.log"
-
-mono /home/rillopy/StandardCrawler/StandardCrawler/bin/Debug/StandardCrawler.exe > "$_file" 2>/dev/null
